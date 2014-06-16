17f65ae116988c88563da5007a7a08f0c144aad3
Jakub Skokan <jakub.skokan@havefun.cz>
2014-06-16 10:06:07 +0200

    Update czech translation

--- locale/ZIMA-PS2PDF_cs_CZ.ts
+++ locale/ZIMA-PS2PDF_cs_CZ.ts
@@ -1,7 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="cs_CZ">
-<defaultcodec>UTF-8</defaultcodec>
+<TS version="2.1" language="cs_CZ">
 <context>
     <name>AboutDialog</name>
     <message>
@@ -10,19 +9,47 @@
         <translation>O programu ZIMA-PS2PDF</translation>
     </message>
     <message>
-        <location filename="../src/AboutDialog.ui" line="54"/>
+        <location filename="../src/AboutDialog.ui" line="58"/>
         <source>&amp;Close</source>
         <translation>&amp;Zavřít</translation>
     </message>
     <message>
-        <location filename="../src/AboutDialog.cpp" line="21"/>
+        <location filename="../src/AboutDialog.cpp" line="41"/>
         <source>&lt;h1&gt;ZIMA-PS2PDF&lt;/h1&gt;&lt;p class=&quot;version&quot;&gt;%1&lt;/p&gt;&lt;p&gt;ZIMA-PS2PDF was created by &lt;a href=&quot;http://www.zima-engineering.cz/&quot;&gt;ZIMA-Engineering&lt;/a&gt; and is released under the &lt;a href=&quot;http://www.gnu.org/&quot;&gt;GNU/GPLv3&lt;/a&gt; license.&lt;/p&gt;&lt;h2&gt;Authors:&lt;/h2&gt;</source>
         <translation>&lt;h1&gt;ZIMA-PS2PDF&lt;/h1&gt;&lt;p class=&quot;version&quot;&gt;%1&lt;/p&gt;&lt;p&gt;ZIMA-PS2PDF byl vytvořen firmou &lt;a href=&quot;http://www.zima-engineering.cz/&quot;&gt;ZIMA-Engineering&lt;/a&gt; a je vydán pod licencí &lt;a href=&quot;http://www.gnu.org/&quot;&gt;GNU/GPLv3&lt;/a&gt;.&lt;/p&gt;&lt;h2&gt;Autoři:&lt;/h2&gt;</translation>
     </message>
     <message>
-        <location filename="../src/AboutDialog.cpp" line="29"/>
+        <location filename="../src/AboutDialog.cpp" line="49"/>
         <source>&lt;ul&gt;&lt;li&gt;Developed by Jakub Skokan &amp;lt;&lt;a href=&quot;mailto:aither@havefun.cz&quot;&gt;aither@havefun.cz&lt;/a&gt;&amp;gt;&lt;/li&gt;&lt;li&gt;Icon created by Aleš Kocur &amp;lt;&lt;a href=&quot;mailto:kafe@havefun.cz&quot;&gt;kafe@havefun.cz&lt;/a&gt;&amp;gt;&lt;/li&gt;</source>
         <translation>&lt;ul&gt;&lt;li&gt;Vyvinul Jakub Skokan &amp;lt;&lt;a href=&quot;mailto:aither@havefun.cz&quot;&gt;aither@havefun.cz&lt;/a&gt;&amp;gt;&lt;/li&gt;&lt;li&gt;Ikonku vytvořil Aleš Kocur &amp;lt;&lt;a href=&quot;mailto:kafe@havefun.cz&quot;&gt;kafe@havefun.cz&lt;/a&gt;&amp;gt;&lt;/li&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>ErrorDialog</name>
+    <message>
+        <location filename="../src/ErrorDialog.ui" line="14"/>
+        <source>Convert errors</source>
+        <translation>Chyby při převodu</translation>
+    </message>
+    <message>
+        <location filename="../src/ErrorDialog.ui" line="24"/>
+        <source>The following files failed to convert:</source>
+        <translation>Selhal převod následujících souborů:</translation>
+    </message>
+    <message>
+        <location filename="../src/ErrorDialog.ui" line="38"/>
+        <source>Do you want to try to convert these files again?</source>
+        <translation>Zkusit převod těchto souborů znovu?</translation>
+    </message>
+    <message>
+        <location filename="../src/ErrorDialog.ui" line="60"/>
+        <source>&amp;Yes</source>
+        <translation>&amp;Ano</translation>
+    </message>
+    <message>
+        <location filename="../src/ErrorDialog.ui" line="67"/>
+        <source>&amp;No</source>
+        <translation>&amp;Ne</translation>
     </message>
 </context>
 <context>
@@ -33,83 +60,83 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/MainWindow.ui" line="30"/>
+        <location filename="../src/MainWindow.ui" line="34"/>
         <source>&amp;Settings</source>
         <translation>&amp;Nastavení</translation>
     </message>
     <message>
-        <location filename="../src/MainWindow.ui" line="50"/>
+        <location filename="../src/MainWindow.ui" line="54"/>
         <source>&amp;Remove selected files from list</source>
         <translation>Odst&amp;ranit vybrané soubory ze seznamu</translation>
     </message>
     <message>
-        <location filename="../src/MainWindow.ui" line="57"/>
+        <location filename="../src/MainWindow.ui" line="61"/>
         <source>&amp;Add files</source>
         <translation>&amp;Přidat soubory</translation>
     </message>
     <message>
-        <location filename="../src/MainWindow.ui" line="66"/>
+        <location filename="../src/MainWindow.ui" line="70"/>
         <source>Cr&amp;eate PDF files in different directory</source>
         <translation>&amp;Vytvořit PDF soubory v jiné složce</translation>
     </message>
     <message>
-        <location filename="../src/MainWindow.ui" line="75"/>
+        <location filename="../src/MainWindow.ui" line="79"/>
         <source>Path</source>
         <translation>Cesta</translation>
     </message>
     <message>
-        <location filename="../src/MainWindow.ui" line="85"/>
+        <location filename="../src/MainWindow.ui" line="89"/>
         <source>...</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/MainWindow.ui" line="95"/>
+        <location filename="../src/MainWindow.ui" line="99"/>
         <source>&amp;Delete source files after conversion</source>
         <translation>O&amp;dstranit zdrojové soubory po převodu</translation>
     </message>
     <message>
-        <location filename="../src/MainWindow.ui" line="117"/>
+        <location filename="../src/MainWindow.ui" line="121"/>
         <source>&amp;Convert</source>
         <translation>Př&amp;evést</translation>
     </message>
     <message>
-        <location filename="../src/MainWindow.ui" line="124"/>
+        <location filename="../src/MainWindow.ui" line="128"/>
         <source>A&amp;bout...</source>
         <translation>O pro&amp;gramu...</translation>
     </message>
     <message>
-        <location filename="../src/MainWindow.cpp" line="93"/>
+        <location filename="../src/MainWindow.cpp" line="115"/>
         <source>Select files</source>
         <translation>Vyberte soubory</translation>
     </message>
     <message>
-        <location filename="../src/MainWindow.cpp" line="169"/>
-        <location filename="../src/MainWindow.cpp" line="294"/>
+        <location filename="../src/MainWindow.cpp" line="191"/>
+        <location filename="../src/MainWindow.cpp" line="293"/>
         <source>Convert</source>
         <translation>Převést</translation>
     </message>
     <message>
-        <location filename="../src/MainWindow.cpp" line="176"/>
+        <location filename="../src/MainWindow.cpp" line="198"/>
         <source>Invalid path to PS2PDF</source>
         <translation>Neplatná cesta k PS2PDF</translation>
     </message>
     <message>
-        <location filename="../src/MainWindow.cpp" line="176"/>
+        <location filename="../src/MainWindow.cpp" line="198"/>
         <source>Please specify valid path to PS2PDF.</source>
         <translation>Prosím zadejte správnou cestu k PS2PDF.</translation>
     </message>
     <message>
-        <location filename="../src/MainWindow.cpp" line="179"/>
+        <location filename="../src/MainWindow.cpp" line="201"/>
         <source>No files to convert</source>
         <translation>Nevybrány žádné soubory k převodu</translation>
     </message>
     <message>
-        <location filename="../src/MainWindow.cpp" line="179"/>
+        <location filename="../src/MainWindow.cpp" line="201"/>
         <source>First select some files to convert.</source>
         <translation>Nejprve vyberte soubory, které chcete převést.</translation>
     </message>
     <message>
-        <location filename="../src/MainWindow.cpp" line="203"/>
+        <location filename="../src/MainWindow.cpp" line="224"/>
         <source>Stop</source>
         <translation></translation>
     </message>
@@ -122,48 +149,56 @@
         <translation>ZIMA-PS2PDF Nastavení</translation>
     </message>
     <message>
-        <location filename="../src/SettingsDialog.ui" line="20"/>
+        <location filename="../src/SettingsDialog.ui" line="24"/>
         <source>Enable system context menu</source>
         <translation>Aktivovat kontextové menu v systému</translation>
     </message>
     <message>
-        <location filename="../src/SettingsDialog.ui" line="29"/>
+        <location filename="../src/SettingsDialog.ui" line="33"/>
         <source>PS2PDF path</source>
         <translation>Cesta k PS2PDF</translation>
     </message>
     <message>
-        <location filename="../src/SettingsDialog.ui" line="39"/>
+        <location filename="../src/SettingsDialog.ui" line="43"/>
         <source>...</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/SettingsDialog.ui" line="50"/>
+        <location filename="../src/SettingsDialog.ui" line="54"/>
         <source>Simultaneous conversions</source>
         <translation>Počet paralelních převodů</translation>
     </message>
     <message>
-        <location filename="../src/SettingsDialog.ui" line="63"/>
+        <location filename="../src/SettingsDialog.ui" line="67"/>
         <source> processes</source>
         <translation> procesů</translation>
     </message>
     <message>
-        <location filename="../src/SettingsDialog.ui" line="96"/>
+        <location filename="../src/SettingsDialog.ui" line="100"/>
         <source>&amp;Close</source>
         <translation>&amp;Zavřít</translation>
     </message>
     <message>
-        <location filename="../src/SettingsDialog.cpp" line="68"/>
-        <location filename="../src/SettingsDialog.cpp" line="74"/>
         <location filename="../src/SettingsDialog.cpp" line="88"/>
-        <location filename="../src/SettingsDialog.cpp" line="103"/>
-        <location filename="../src/SettingsDialog.cpp" line="113"/>
+        <location filename="../src/SettingsDialog.cpp" line="94"/>
+        <location filename="../src/SettingsDialog.cpp" line="108"/>
+        <location filename="../src/SettingsDialog.cpp" line="123"/>
+        <location filename="../src/SettingsDialog.cpp" line="133"/>
         <source>Open with ZIMA-PS2PDF</source>
         <translation>Otevřit se ZIMA-PS2PDF</translation>
     </message>
     <message>
-        <location filename="../src/SettingsDialog.cpp" line="124"/>
+        <location filename="../src/SettingsDialog.cpp" line="144"/>
         <source>Select PS2PDF executable</source>
         <translation>Vyberte spustitelný soubor PS2PDF</translation>
+    </message>
+</context>
+<context>
+    <name>Worker</name>
+    <message>
+        <location filename="../src/Worker.cpp" line="66"/>
+        <source>convert failed</source>
+        <translation>převod selhal</translation>
     </message>
 </context>
 </TS>
