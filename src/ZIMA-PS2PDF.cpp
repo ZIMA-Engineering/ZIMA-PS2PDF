91ecf845ead3fd7f7cfe8e42f751556c05dd84bb
Jakub Skokan <jakub.skokan@havefun.cz>
2019-09-27 09:49:46 +0200

    Search for translation files in locale/

--- src/ZIMA-PS2PDF.cpp
+++ src/ZIMA-PS2PDF.cpp
@@ -65,6 +65,7 @@ int main(int argc, char *argv[])
 		paths
 				<< filename
 				<< QApplication::applicationDirPath() + "/" + filename
+				<< QApplication::applicationDirPath() + "/locale/" + filename
 				<< ("locale/" + filename)
 				<< (":/" + filename);
 
